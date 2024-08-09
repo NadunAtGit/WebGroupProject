3fffc1e45215e5478f7adf96fa398530ca837850
pavithra614 <nilupul-im21002@stu.kln.ac.lk>
2024-08-09 19:37:39 +0530

    Change the CSS Link

--- Dashboard/Dashboard.php
+++ Dashboard/Dashboard.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Responsive Admin Dashboard | Korsat X Parmaga</title>
     <!-- ======= Styles ====== -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="Dashboard.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
