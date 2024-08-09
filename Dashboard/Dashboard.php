7b36dec8dc9c532958db0c72a21df80f3aaec5df
pavithra614 <nilupul-im21002@stu.kln.ac.lk>
2024-08-09 19:43:46 +0530

    Responsive Features added(DasboardScript.js added)

--- Dashboard/Dashboard.php
+++ Dashboard/Dashboard.php
@@ -194,7 +194,7 @@
     </div>
 
     <!-- =========== Scripts =========  -->
-    <script src="assets/js/main.js"></script>
+    <script src="DashboardScript.js"></script>
 
     <!-- ====== ionicons ======= -->
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
